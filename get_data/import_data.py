747ff73c470fc567e9fd18aa6dd4eb29e8ac6f4e
Dr B <Brandon.borde@gmail.com>
2023-01-30 21:39:31 -0500

    Create data import and config

--- get_data/import_data.py
+++ get_data/import_data.py
@@ -2,6 +2,10 @@ import pandas as pd
 import requests
 import tweepy
 
+def construct_twitter_url(search_terms=["pharma"]) -> str:
+    complete_url = ",".join(search_terms)
+    return complete_url
+
 def get_twitter_data():
     twt_df = pd.read_csv()
     return twt_df
